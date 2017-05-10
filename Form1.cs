fa3947acead63c49441d4b87fe2a70ec9be1ca0f
WIN_NT\x16j039 <x16j039@chiba-fjb.ac.jp>
2017-05-10 12:11:40 +0900

    10倍

--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,17 @@ namespace Example4
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            int iValue;
+            iValue = int.Parse(tbxNum.Text);
+            lblAns.Text = String.Format("{0}", iValue * 10);
         }
     }
 }
